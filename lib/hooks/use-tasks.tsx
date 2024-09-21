da21561477e3d85aa65b74356e17ba7377e11777
Jason Miller <jasonm36@uw.edu>
2024-09-21 15:41:54 -0700

    added alert to addProblem

--- lib/hooks/use-tasks.tsx
+++ lib/hooks/use-tasks.tsx
@@ -110,6 +110,7 @@ export const TasksProvider = ({ children }: { children: ReactNode }) => {
 
   const addProblem = (problemText: string, explanation: string, answer: string) => {
     setProblems([...problems, {problemText: problemText, explanation: explanation, answer: answer}]);
+    alert(`addProblem called with problem ${problemText}`);
   }
 
   const addTask = (title: string) => {
