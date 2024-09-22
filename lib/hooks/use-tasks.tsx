084b41fdf0d67c979433e7f53da96d3ceadd9e9f
Marcus Christerson <marcus.christerson@gmail.com>
2024-09-22 12:25:39 -0700

    uncomment useCopilotAction

--- lib/hooks/use-tasks.tsx
+++ lib/hooks/use-tasks.tsx
@@ -221,7 +221,7 @@ export const AppProvider = ({ children }: { children: ReactNode }) => {
     })
 
     // ADDING TO COPILOTACTIONS
-    /*useCopilotAction({
+    useCopilotAction({
         name: "createProblems",
         description: "Append to the current list with a new problems using the provided topics, number of questions, and the found answers. The topics should be equally represented in the problem set. The answers should be represented as strings.",
         parameters: [
@@ -252,7 +252,7 @@ export const AppProvider = ({ children }: { children: ReactNode }) => {
         handler: ({ problems }) => {
             setAproblemSet(problems)
         }
-    });*/
+    });
     // useCopilotAction({
 
     // })
